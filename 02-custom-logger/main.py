9e392d15e3fdf5da5cb335c6549f4c2dacd51877
Benedict Meinke <Benedict_Meinke@web.de>
2026-08-07 21:35:12 +0200

    [ADD] custom logger.

---
+++ 02-custom-logger/main.py
@@ -0,0 +1,27 @@
+import logging
+from firstChild import first_logs
+from secondChild import second_logs
+
+console_handler: logging.StreamHandler = logging.StreamHandler()
+file_handler: logging.FileHandler = logging.FileHandler('custom-logger.log')
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s %(name)s %(levelname)s: %(message)s",
+    datefmt="[%d.%m.%Y %X]",
+    handlers=[console_handler, file_handler]
+)
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+def log_messages():
+    logger.debug('This is a simple DEBUG log.')
+    logger.info('This is a simple INFO log.')
+    logger.warning('This is a simple WARNING log.')
+    logger.error('This is a simple ERROR log.')
+    logger.critical('This is a simple CRITICAL log.')
+
+if __name__ == '__main__':
+    log_messages()
+    first_logs()
+    second_logs()
